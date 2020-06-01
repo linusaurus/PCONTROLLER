4cffd30b1516d1b0c37b0ad3c4e02cf7701898d1
Richard Young <richard@weaselware.biz>
2020-06-01 15:01:10 -0700

    Corrected funky message switching

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,54 @@
 #include <Arduino.h>
 #include <SPI.h>
-#include <Ethernet.h>
-#include <Automaton.h>
 #include <PubSubClient.h>
-#include <ArduinoJson.h>
+#include <UIPEthernet.h>
 
-// Update these with values suitable for your network.
-byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xDD };
-IPAddress ip(192, 168, 10, 100);
-IPAddress server(192, 168, 10, 22);
+#define BUF_SIZE 512
 
-// ------------------------------------------------------------
-Atm_led led;
+byte mac[] = { 0x00, 0x04, 0xA3, 0x21, 0xCA, 0x38 };   // Nanode MAC address.
+byte server[] = { 192,168, 10, 3 };                    // Set MQTT broker server IP address
+byte ip[]     = { 192,168, 10, 117 };                  // Set the IP address for the arduino nano mcu 
+
+
+
+String msgString;                                          // Initialize msgString variable
 
 void callback(char* topic, byte* payload, unsigned int length) {
+  
+  // Handle message arrived upon a publish from a MQTT client
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
-  for (unsigned int i=0;i<length;i++) {
+  
+ for (unsigned int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
-  Serial.println();
+  
+      
+
+ if ((char)payload[0] == '0') {
+    
+    Serial.println("MQTT_STOP");
+  
+  } 
+
+  if ((char)payload[0] == '1') {
+   
+    
+    Serial.println("MQTT_CLOSING");
+    
+   
+  } 
+
 }
 
 EthernetClient ethClient;
-PubSubClient client(ethClient);
+PubSubClient client(server, 1883, callback, ethClient);     // Initialize a MQTT client instance
 
 void reconnect() {
-
-   led.trigger(led.EVT_ON);
   // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-   
     // Attempt to connect
     if (client.connect("arduinoClient")) {
       Serial.println("connected");
@@ -40,7 +56,6 @@ void reconnect() {
       client.publish("outTopic","hello world");
       // ... and resubscribe
       client.subscribe("inTopic");
-      led.trigger(led.EVT_BLINK);
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -51,19 +66,24 @@ void reconnect() {
   }
 }
 
-void setup() {
-  Serial.begin(9600);
+
+  void setup()
+{
+  Serial.begin(57600);
 
   client.setServer(server, 1883);
   client.setCallback(callback);
-  led.begin(LED_BUILTIN);
+
   Ethernet.begin(mac, ip);
   // Allow the hardware to sort itself out
   delay(1500);
 }
 
-void loop() {
-  if (!client.connected()) {
+
+
+void loop()
+{
+ if (!client.connected()) {
     reconnect();
   }
   client.loop();
